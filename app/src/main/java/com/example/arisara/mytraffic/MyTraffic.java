614fecd328775d4246aa86420400f506fbff5db2
Mai <arismaaii.2@gmail.com>
2015-12-26 15:01:56 +0700

    finish 1 day

--- app/src/main/java/com/example/arisara/mytraffic/MyTraffic.java
+++ app/src/main/java/com/example/arisara/mytraffic/MyTraffic.java
@@ -1,13 +1,61 @@
 package com.example.arisara.mytraffic;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MyTraffic extends AppCompatActivity {
+    //explicit การประกาศตัวแปร
+    private ListView TrafficListView;
+    private Button AboutMeButton;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_traffic);
+
+        //Bind Widget
+        bindWidget();
+
+        //button controller
+        buttonController();
+
+
+
+    }//Main Method กลุ่มของคำสั่ง จบด้วย ;
+
+    private void buttonController() {
+        AboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // sound effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.lion);
+                buttonMediaPlayer.start();
+
+            }// event
+        });
+    }// buttonController
+
+    private void bindWidget() {
+        TrafficListView = (ListView) findViewById(R.id.listView);
+        AboutMeButton = (Button) findViewById(R.id.button);
     }
-}
+
+
+
+}//Main Class
